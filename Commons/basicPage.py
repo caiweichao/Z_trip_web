5c12f40b51f5b70d2f694100537ae96ac9016fea
yiyy <1121948183@qq.com>
2019-09-17 23:23:24 +0800

    优化编码规范

--- Commons/basicPage.py
+++ Commons/basicPage.py
@@ -16,16 +16,17 @@ class BasicPage:
     def __init__(self, driver):
         self.driver = driver
         self.driver = webdriver.Chrome()
+        self.driver.get('https://www.baidu.com')
 
     # 等待元素可见
     def wait_element_visible(self, locator, timeout=Contans.basic_timeout,
-                             Polling_Interval=Contans.basic_Polling_Interval):
+                             pollinginterval=Contans.basic_Polling_Interval):
         log.info('等待元素: {}可见'.format(locator))
         try:
             # 获取开始等待时间具体到秒
             start_time = datetime.datetime.now().second
             # 默认超时等待时间为30秒,间隔0.5秒轮询一次可以根据配置文件修改超时时间和轮询时间
-            WebDriverWait(self.driver, timeout=timeout, poll_frequency=Polling_Interval).until(
+            WebDriverWait(self.driver, timeout=timeout, poll_frequency=pollinginterval).until(
                 ec.visibility_of_element_located(locator))
             # 计算共计等待时间
             wait_time = datetime.datetime.now().second - start_time
@@ -34,7 +35,7 @@ class BasicPage:
             # 获取超时时间戳
             tag_time = time.time()
             # 截图并且保存
-            self.save_webImg(tag_time=tag_time)
+            self.save_webimg(tag_time=tag_time)
             log.error("等待元素超时,页面已经截图并且保存文件名:{}".format(tag_time))
             raise e
         except InvalidSelectorException as e:
@@ -42,13 +43,13 @@ class BasicPage:
 
     # 等待元素存在
     def wait_element_exist(self, locator, timeout=Contans.basic_timeout,
-                           Polling_Interval=Contans.basic_Polling_Interval):
+                           pollinginterval=Contans.basic_Polling_Interval):
         log.info('等待元素: {}存在'.format(locator))
         try:
             # 获取开始等待时间具体到秒
             start_time = datetime.datetime.now().second
             # 默认超时等待时间为30秒,间隔0.5秒轮询一次可以根据配置文件修改超时时间和轮询时间
-            WebDriverWait(self.driver, timeout=timeout, poll_frequency=Polling_Interval).until(
+            WebDriverWait(self.driver, timeout=timeout, poll_frequency=pollinginterval).until(
                 ec.presence_of_element_located(locator))
             # 计算共计等待时间
             wait_time = datetime.datetime.now().second - start_time
@@ -57,7 +58,7 @@ class BasicPage:
             # 获取超时时间戳
             tag_time = time.time()
             # 截图并且保存
-            self.save_webImg(tag_time=tag_time)
+            self.save_webimg(tag_time=tag_time)
             log.error("等待元素超时,页面已经截图并且保存文件名:{}".format(tag_time))
             raise e
         except InvalidSelectorException as e:
@@ -65,12 +66,12 @@ class BasicPage:
 
     # 等待元素不可见
     def wait_element_not_visible(self, locator, timeout=Contans.basic_timeout,
-                                 Polling_Interval=Contans.basic_Polling_Interval):
+                                 pollinginterval=Contans.basic_Polling_Interval):
         log.info('等待元素: {}消失不可见'.format(locator))
         try:
             # 获取开始等待时间具体到秒
             start_time = datetime.datetime.now().second
-            WebDriverWait(self.driver, timeout=timeout, poll_frequency=Polling_Interval).until_not(
+            WebDriverWait(self.driver, timeout=timeout, poll_frequency=pollinginterval).until_not(
                 ec.visibility_of_element_located(locator))
             # 计算共计等待时间
             wait_time = datetime.datetime.now().second - start_time
@@ -79,84 +80,96 @@ class BasicPage:
             # 获取超时时间戳
             tag_time = time.time()
             # 截图并且保存
-            self.save_webImg(tag_time=tag_time)
+            self.save_webimg(tag_time=tag_time)
             log.error("等待元素超时,页面已经截图并且保存文件名:{}".format(tag_time))
             raise e
         except InvalidSelectorException as e:
             log.error("等待或定位表达式异常\n {}".format(e))
 
     # 定位元素
-    def find_element(self, model, locator, type='visible'):
+    def find_element(self, model, locator, mode='visible'):
         """
         :param model: 传参定位的是哪个页面 字符串形式
         :param locator: 元素的定位表达式 例:(By.xx,'定位表达式')
-        :param type: visible(元素可见),notvisible(元素消失不可见), exist(元素存在)
+        :param mode: visible(元素可见),notvisible(元素消失不可见), exist(元素存在)
         :return:
         """
         # 判断元素定位使用的是那种等待方式
-        if type == 'visible':
+        if mode == 'visible':
             self.wait_element_visible(locator)
-        elif type == 'notvisible':
+        elif mode == 'notvisible':
             self.wait_element_not_visible(locator)
-        elif type == 'exist':
+        elif mode == 'exist':
             self.wait_element_exist(locator)
         else:
-            log.error('type参数传值异常,入参值为：{}'.format(type))
+            log.error('type参数传值异常,入参值为：{}'.format(mode))
         try:
             log.info('正在查找{}页面属性为: {} 的元素'.format(model, locator))
             # 元素定位传入动态参数
-            self.driver.find_element(*locator)
+            element = self.driver.find_element(*locator)
+            return element
         except Exception as e:
             # 获取超时时间戳
             tag_time = time.time()
             # 截图并且保存
-            self.save_webImg(tag_time=tag_time)
+            self.save_webimg(tag_time=tag_time)
             log.error("查找元素失败,页面已经截图并且保存文件名:{}".format(tag_time))
             raise e
 
     # 定位一组元素
-    def find_elements(self, model, locator, type='visible'):
+    def find_elements(self, model, locator, mode='visible'):
         """
         :param model: 传参定位的是哪个页面 字符串形式
         :param locator: 元素的定位表达式 例:(By.xx,'定位表达式')
-        :param type: visible(元素可见),notvisible(元素消失不可见), exist(元素存在)
-        :return:
+        :param mode: visible(元素可见),notvisible(元素消失不可见), exist(元素存在)
         """
         # 判断元素定位使用的是那种等待方式
-        if type == 'visible':
+        if mode == 'visible':
             self.wait_element_visible(locator)
-        elif type == 'notvisible':
+        elif mode == 'notvisible':
             self.wait_element_not_visible(locator)
-        elif type == 'exist':
+        elif mode == 'exist':
             self.wait_element_exist(locator)
         else:
-            log.error('type参数传值异常,入参值为：{}'.format(type))
+            log.error('type参数传值异常,入参值为：{}'.format(mode))
         try:
             log.info('正在查找{}页面属性为: {} 的元素'.format(model, locator))
             # 元素定位传入动态参数
-            self.driver.find_element(*locator)
+            elements = self.driver.find_elements(*locator)
+            return elements
         except Exception as e:
             # 获取超时时间戳
             tag_time = time.time()
             # 截图并且保存
-            self.save_webImg(tag_time=tag_time)
+            self.save_webimg(tag_time=tag_time)
             log.error("查找元素失败,页面已经截图并且保存文件名:{}".format(tag_time))
             raise e
 
     # 点击元素
-    def click_element(self):
-        pass
-
+    def click_element(self, model, locator, mode='visible'):
+        log.info('尝试点击:{}页面,属性为{}的元素'.format(model, locator))
+        # 查找需要点击的元素
+        element = self.find_element(model, locator, mode)
+        try:
+            log.info('点击操作:{}页面下的属性为: {}的元素'.format(model, locator))
+            element.click()
+        except Exception:
+            log.error('页面{}的属性: {} 点击失败请检查'.format(model, locator))
+            # 获取点击失败时候的时间戳并且截图
+            tag_time = time.time()
+            self.save_webimg(tag_time)
+            raise
 
     # 输入文本内容
     def input_text(self):
         pass
+
     # 获取元素的文本内容
 
+    # 句柄切换
+
     # 异常截图并且存储
-    def save_webImg(self, tag_time):
-        # 日志文件夹存放实例化
-        LogManagement()
+    def save_webimg(self, tag_time):
         # 创建图片存储文件夹如果存在就不创建,并且删除七天之前的图片文件夹
         imgdir = LogManagement().get_log_dir(logs_path=Contans.pt_log)
         # 图片文件名称= 文件夹路径+模块名称_当前时间的时间戳.png
@@ -166,7 +179,7 @@ class BasicPage:
             self.driver.save_screenshot(imgPath)
             log.info('截图成功,文件名称为: {} '.format(imgPath))
         except Exception as e:
-            log.error('截图失败请检查')
+            log.error('截图失败请检查\n{}'.format(e))
             raise e
         return imgPath
 
--- Demo/demo.py
+++ Demo/demo.py
@@ -1,48 +1,50 @@
-import openpyxl
+# import openpyxl
+#
+#
+# class TIMU:
+#     # 初始化题目
+#     def __init__(self):
+#         self.daan = None
+#         self.timu = None
+#         self.xxA = None
+#         self.xxB = None
+#         self.xxC = None
+#         self.xxD = None
+# class newtimu:
+#     def __init__(self):
+#         self.timi =None
+#         self.daan = None
+#         self.wanzen = None
+#
+#
+# class DoExcel:
+#     # 打开文件
+#     def __init__(self, filename):
+#         self.filename = filename
+#         self.workbook = openpyxl.load_workbook(filename=filename)
+#
+#     def get_timu(self, sheet_name):
+#         sheet = self.workbook[sheet_name]
+#         max_row = sheet.max_row
+#         timus = []
+#         for r in range(2, max_row + 1):
+#             timu = TIMU()
+#             new_timu =newtimu()
+#             timu.timu = sheet.cell(row=r, column=2).value
+#             timu.daan = sheet.cell(row=r, column=1).value
+#             timu.xxA = sheet.cell(row=r, column=3).value
+#             timu.xxB = sheet.cell(row=r, column=4).value
+#             timu.xxC = sheet.cell(row=r, column=5).value
+#             timu.xxD = sheet.cell(row=r, column=6).value
+#             newtimu.timi = '答案'+str(timu.daan)+"  "+str(timu.timu)
+#             newtimu.daan = 'A: '+str(timu.xxA)+"  B: "+str(timu.xxB)+"  C: "+str(timu.xxC)+"  D: "+str(timu.xxD)
+#             newtimu.wanzen = newtimu.timi+"\n"+newtimu.daan+"\n"
+#             print(newtimu.wanzen)
+#             timus.append(newtimu)
+#         return timus
+# if __name__ == '__main__':
+#         do = DoExcel(filename="D:\Python_project\Z_trip_web\Demo\\123.xlsx")
+#         timu = do.get_timu('Sheet1')
+#
 
-
-class TIMU:
-    # 初始化题目
-    def __init__(self):
-        self.daan = None
-        self.timu = None
-        self.xxA = None
-        self.xxB = None
-        self.xxC = None
-        self.xxD = None
-class newtimu:
-    def __init__(self):
-        self.timi =None
-        self.daan = None
-        self.wanzen = None
-
-
-class DoExcel:
-    # 打开文件
-    def __init__(self, filename):
-        self.filename = filename
-        self.workbook = openpyxl.load_workbook(filename=filename)
-
-    def get_timu(self, sheet_name):
-        sheet = self.workbook[sheet_name]
-        max_row = sheet.max_row
-        timus = []
-        for r in range(2, max_row + 1):
-            timu = TIMU()
-            new_timu =newtimu()
-            timu.timu = sheet.cell(row=r, column=2).value
-            timu.daan = sheet.cell(row=r, column=1).value
-            timu.xxA = sheet.cell(row=r, column=3).value
-            timu.xxB = sheet.cell(row=r, column=4).value
-            timu.xxC = sheet.cell(row=r, column=5).value
-            timu.xxD = sheet.cell(row=r, column=6).value
-            newtimu.timi = '答案'+str(timu.daan)+"  "+str(timu.timu)
-            newtimu.daan = 'A: '+str(timu.xxA)+"  B: "+str(timu.xxB)+"  C: "+str(timu.xxC)+"  D: "+str(timu.xxD)
-            newtimu.wanzen = newtimu.timi+"\n"+newtimu.daan+"\n"
-            print(newtimu.wanzen)
-            timus.append(newtimu)
-        return timus
-if __name__ == '__main__':
-        do = DoExcel(filename="D:\Python_project\Z_trip_web\Demo\\123.xlsx")
-        timu = do.get_timu('Sheet1')
-
+from Commons.MysqlConnect import Mysql_Util
